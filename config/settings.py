fe2fde98e3ce5942987bd4195b0b6b552756f92e
Sugadeve <82795438+SUGADEVE776@users.noreply.github.com>
2024-06-12 21:28:53 +0530

    Changed grid into config folder

--- config/settings.py
+++ config/settings.py
@@ -45,7 +45,7 @@ MIDDLEWARE = [
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
 ]
 
-ROOT_URLCONF = "grid.urls"
+ROOT_URLCONF = "config.urls"
 
 TEMPLATES = [
     {
@@ -63,7 +63,7 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = "grid.wsgi.application"
+WSGI_APPLICATION = "config.wsgi.application"
 
 
 # Database
